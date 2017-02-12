d5304e533a071c53718a0f47eace537c59dd84d9
wmikefish <wmikefish@gmail.com>
2017-02-12 22:42:36 +0900

    [add] add test pattern of dencrypt
    [add-test] add bruteforce test of ROT0~26

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -2,7 +2,69 @@ require "spec_helper"
 require "caesar"
 
 describe Caesar do
-  it "EHOLのROT13がRUBYになること" do
-    expect(Caesar.dencrypt("EHOL", 13)).to eq 'RUBY'
+# ROTn=rotate by n placesで、n文字分回すという意味
+
+ROTN = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        'BCDEFGHIJKLMNOPQRSTUVWXYZA',
+        'CDEFGHIJKLMNOPQRSTUVWXYZAB',
+        'DEFGHIJKLMNOPQRSTUVWXYZABC',
+        'EFGHIJKLMNOPQRSTUVWXYZABCD',
+        'FGHIJKLMNOPQRSTUVWXYZABCDE',
+        'GHIJKLMNOPQRSTUVWXYZABCDEF',
+        'HIJKLMNOPQRSTUVWXYZABCDEFG',
+        'IJKLMNOPQRSTUVWXYZABCDEFGH',
+        'JKLMNOPQRSTUVWXYZABCDEFGHI',
+        'KLMNOPQRSTUVWXYZABCDEFGHIJ',
+        'LMNOPQRSTUVWXYZABCDEFGHIJK',
+        'MNOPQRSTUVWXYZABCDEFGHIJKL',
+        'NOPQRSTUVWXYZABCDEFGHIJKLM',
+        'OPQRSTUVWXYZABCDEFGHIJKLMN',
+        'PQRSTUVWXYZABCDEFGHIJKLMNO',
+        'QRSTUVWXYZABCDEFGHIJKLMNOP',
+        'RSTUVWXYZABCDEFGHIJKLMNOPQ',
+        'STUVWXYZABCDEFGHIJKLMNOPQR',
+        'TUVWXYZABCDEFGHIJKLMNOPQRS',
+        'UVWXYZABCDEFGHIJKLMNOPQRST',
+        'VWXYZABCDEFGHIJKLMNOPQRSTU',
+        'WXYZABCDEFGHIJKLMNOPQRSTUV',
+        'XYZABCDEFGHIJKLMNOPQRSTUVW',
+        'YZABCDEFGHIJKLMNOPQRSTUVWX',
+        'ZABCDEFGHIJKLMNOPQRSTUVWXY',
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+
+#　dencrypt
+  it "EHOLがROT13でRUBYになること" do
+    expect(Caesar.dencrypt('EHOL', 13)).to eq 'RUBY'
   end
+
+  it "RUBYがROT13でEHOLになること" do
+    expect(Caesar.dencrypt('RUBY', 13)).to eq 'EHOL'
+  end
+
+  it "ROT13を２回かけると元に戻ること" do
+    ciphertext = 'RUBY'
+    rot13text = Caesar.dencrypt(ciphertext, 13)
+    expect(Caesar.dencrypt(rot13text, 13)).to eq ciphertext
+  end
+
+  it "IBMがROT25でHALになること" do
+    expect(Caesar.dencrypt('IBM', 25)).to eq 'HAL'
+  end
+
+  it "HALがROT1でIBMになること" do
+    expect(Caesar.dencrypt('HAL', 1)).to eq 'IBM'
+  end
+
+  it "ROT26で元にもどること" do
+    ciphertext = 'HAL'
+    expect(Caesar.dencrypt(ciphertext, 26)).to eq ciphertext
+  end
+
+  it "ALHABETSのROT0~26が全て正しいこと" do
+    0...ROTN.length do |i|
+      expect(Caesar.dencrypt(ciphertext, i)).to eq ROTN[i]
+    end
+  end
+
+
 end
